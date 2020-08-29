4f2261a5c6d7e3e4fc35f9dedc8f013896062603
Adam Migus <adam@migus.org>
2020-08-28 20:39:55 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-tss-sdk",
-    version="0.0.1",
+    version="0.0.2",
     author="Adam Migus",
     author_email="adam@migus.org",
     description="The Thycotic Secret Server Python SDK",
